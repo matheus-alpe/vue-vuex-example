5b0825f64c197854085a6ae2fb55467b3525f723
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-29 14:54:10 -0300

    feat: state, mapState e computedProperties

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,25 +1,39 @@
 <template>
     <div>
         <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+        <h4 class="font-weight-light">Contador Alias: {{ contadorAlias }}</h4>
+        <h6 class="font-weight-light">Contador Multiplicado: {{ contadorMultiplicado }}</h6>
         <button class="btn btn-lg btn-danger mr-1" @click="decrementar">-</button>
         <button class="btn btn-lg btn-success" @click="incrementar">+</button>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
     data() {
-        return { 
-            contador: 0
+        return {
+            contadorLocal: 2
         }
     },
+
+    // computed: mapState(['contador']),
+    computed: mapState({
+        // contador: state => state.contador
+        contador: 'contador',
+        contadorAlias: 'contador',
+        contadorMultiplicado(state) {
+            return state.contador * this.contadorLocal
+        }
+    }),
     
     methods: {
         decrementar() {
-            this.contador--;
+            this.$store.state.contador--;
         },
         incrementar() {
-            this.contador++;
+            this.$store.state.contador++;
         }
     }
 }
