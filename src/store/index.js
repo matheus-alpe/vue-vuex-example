1d3409380055358721f5a47798a52123ebcd96c8
Matheus A. Pereira <matttalves@gmail.com>
2021-09-05 11:34:29 -0300

    feat: enable strict mode on project in development

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,12 @@ import contador from '@/resources/contador/_store'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    // Habilitando strict mode não permite realizar alteração sem usar mutation
+    // Habilitar somente em development
+    strict: process.env.NODE_ENV !== 'product',
+
     ...global,
+
     modules: {
         contador
     }
